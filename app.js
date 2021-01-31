4b29436b8b5938d7cf164c5d4dc14040cef69359
nedoder <nedoder89@gmail.com>
2021-01-31 19:12:18 +0100

    added authentication

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const { json, urlencoded } = require("body-parser");
 const users = require("./controllers/users");
 const products = require("./controllers/products");
+const auth = require("./controllers/auth")
 const connect = require("./helper").connect;
 const app = express();
 
@@ -10,52 +11,52 @@ app.use(urlencoded({ extended: true }));
 app.use(json());
 
 //route for creating new product
-app.post("/product", products.createProduct);
+app.post("/product", auth.verifyToken, products.createProduct);
 
 //route for getting product by product name
-app.get("/product/:name", products.findProductByName);
+app.get("/product/:name", auth.verifyToken, products.findProductByName);
 //route for updating product by product name
-app.put("/product/:name", products.updateProductByName);
+app.put("/product/:name", auth.verifyToken, products.updateProductByName);
 //route for deleting product by product name
-app.delete("/product/:name", products.deleteProductByName);
+app.delete("/product/:name", auth.verifyToken, products.deleteProductByName);
 
 //route for getting all products
-app.get("/products", products.findAllProducts);
+app.get("/products", auth.verifyToken, products.findAllProducts);
 
 //route for creating new user
 app.post("/user", users.createUser);
 
 //route for getting user by username
-app.get("/user/:username", users.findUserByUsername);
+app.get("/user/:username", auth.verifyToken, users.findUserByUsername);
 //route for updating user by username
-app.put("/user/:username", users.updateUserByUsername);
+app.put("/user/:username", auth.verifyToken, users.updateUserByUsername);
 //route for deleting user by username
-app.delete("/user/:username", users.deleteUserByUsername);
+app.delete("/user/:username", auth.verifyToken, users.deleteUserByUsername);
 
 
 //route for getting all users
-app.get("/users", users.findAllUsers);
+app.get("/users", auth.verifyToken, users.findAllUsers);
 
 //route for getting product by id
-app.get("/product_id/:id", products.findProductById);
+app.get("/product_id/:id", auth.verifyToken, products.findProductById);
 //route for updating product by id
-app.put("/product_id/:id", products.updateProductById);
+app.put("/product_id/:id", auth.verifyToken, products.updateProductById);
 //route for deleting product by id
-app.delete("/product_id/:id", products.deleteProductById);
+app.delete("/product_id/:id", auth.verifyToken, products.deleteProductById);
 
 //route for decrementing quantity of product chosen by id
-app.put("/product_dec/:id", products.decrementQuantity);
+app.put("/product_dec/:id", auth.verifyToken, products.decrementQuantity);
 //route for incrementing quantity of product chosen by id
-app.put("/product_inc/:id", products.incrementQuantity);
+app.put("/product_inc/:id", auth.verifyToken, products.incrementQuantity);
 //route for getting quantity of product chosen by id
-app.get("/product_num/:id", products.productQuantity);
+app.get("/product_num/:id", auth.verifyToken, products.productQuantity);
 
 app.get("/user");
 app.put("/user");
 app.delete("/user");
 
 //route for logging user
-app.get("/login");
+app.get("/login", auth.generateToken);
 
 
 connect("mongodb://localhost:27017/homework-api")
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,43 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+const User = require("../models/user");
+require("dotenv").config();
+
+//checking if user exists in database and generating token
+async function generateToken(req, res) {
+    try {
+        const user = {
+            username: req.body["username"],
+            password: crypto.createHash('sha256').update(req.body["password"]).digest('hex')
+        }
+        const result = await User.findOne({ username: user.username, password: user.password });
+        if (result) {
+            jwt.sign({ user }, "lalal", { expiresIn: "1h" }, (err, token) => {
+                res.json({ token });
+            });
+        } else {
+            res.status(401).json("Wrong username or password");
+        }
+    } catch (err) {
+        res.status(401).json("Authentication failed");
+    }
+}
+
+//verify token
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403).json({ "error": "Authentication failed!" });;
+    }
+
+}
+
+module.exports = {
+    verifyToken,
+    generateToken
+}
